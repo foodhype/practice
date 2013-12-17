36ec361402cacad0b508bc1c92451467ee51e1df
obsessedone <obsessedone@VB.(none)>
2013-12-17 12:56:08 -0600

    Added Binet's Fibonacci Number Formula

--- algos/fib.py
+++ algos/fib.py
@@ -1,11 +1,16 @@
 def fib(n):
+  """Return nth fibonacci number"""
+  golden_ratio = (1 + 5**0.5) / 2
+  return int((golden_ratio**n - (-golden_ratio)**(-n)) / 5**0.5)
+
+def fibs(n):
   """Generator for first n fibonacci numbers"""
   a, b = 0, 1
   for _ in xrange(n):
     yield a
     a, b = b, a + b
 
-def fib_xrange(stop):
+def fibs_xrange(stop):
   """Generator for fibonacci numbers up to stop"""
   a, b = 0, 1
   while a < stop:
