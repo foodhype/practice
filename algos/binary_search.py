ed9ba48de2f5d408024a399f6071758f9ec84b98
Brian Schmitz <bschmitz@cs.utexas.edu>
2014-09-07 16:37:33 -0500

    added some algorithms

--- algos/binary_search.py
+++ algos/binary_search.py
@@ -1,13 +1,14 @@
 from bisect import bisect_left
 
-def binary_search(li, target):
-  pos = bisect_left(li, target)
-  return pos if pos != len(li) and li[pos] == target else -1
+def binary_search(li, target, lo=0, hi=None):
+    hi = hi if hi is not None else len(li)
+    pos = bisect_left(li, target, lo, hi)
+    return pos if pos != len(li) and li[pos] == target else -1
 
 def main():
-  li = [1, 1, 2, 3, 5, 8]
-  print li
-  print binary_search(li, 3)
+    li = [1, 1, 2, 3, 5, 8]
+    print li
+    print binary_search(li, 3)
 
 if __name__ == "__main__":
   main()
