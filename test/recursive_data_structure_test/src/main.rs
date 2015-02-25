8d8c21a6fd4c6bfc874ad418a7ff5eeec83213e1
foodhype <brianschmitz.main@gmail.com>
2015-02-24 21:12:42 -0600

    Doesn't require enums

--- test/recursive_data_structure_test/src/main.rs
+++ test/recursive_data_structure_test/src/main.rs
@@ -1,58 +1,97 @@
-enum BST<T> {
-    Node(T, Box<BST<T>>, Box<BST<T>>),
-    Leaf,
+use std::cmp::Ordering;
+
+
+struct BST<T> {
+    root: Option<Box<TreeNode<T>>>
 }
 
 
-fn insert<T>(tree: Box<BST<T>>, value: T) -> Box<BST<T>> where T: Ord {
-    let temp = *tree; // hack to get around box destructuring bug in Rust
+impl<T: Ord> BST<T> {
+    fn new() -> BST<T> {
+        BST {
+            root: None
+        }
+    }
 
-    Box::new(
-        match temp {
-            BST::Leaf => {
-                BST::Node(value, Box::new(BST::Leaf), Box::new(BST::Leaf))
-            },
-            BST::Node(root_value, left, right) => {
-                if value < root_value {
-                    BST::Node(root_value, insert(left, value), right)
-                } else {
-                    BST::Node(root_value, left, insert(right, value))
+    fn contains(&self, value: T) -> bool {
+        BST::contains_with(&self.root, value)
+    }
+
+    fn contains_with(root: &Option<Box<TreeNode<T>>>, value: T) -> bool {
+        let mut current: &Option<Box<TreeNode<T>>> = root;
+
+        loop {
+            match *current {
+                Some(ref node) => {
+                    match value.cmp(&node.value) {
+                        Ordering::Less => {
+                            current = &node.left;
+                        },
+                        Ordering::Greater => {
+                            current = &node.right;
+                        },
+                        Ordering::Equal => {
+                            return true
+                        }
+                    }
+                },
+                None => {
+                    return false
                 }
             }
         }
-    )
+    }
+
+    fn insert(&mut self, value: T) {
+        BST::insert_with(&mut self.root, value);
+    }
+
+    fn insert_with(root: &mut Option<Box<TreeNode<T>>>, value: T) {
+        match *root {
+            Some(ref mut node) => {
+                match value.cmp(&node.value) {
+                    Ordering::Less => {
+                        BST::insert_with(&mut node.left, value);
+                    },
+                    Ordering::Greater => {
+                        BST::insert_with(&mut node.right, value);
+                    },
+                    Ordering::Equal => {
+                        // TODO: Replace this println with proper error handling.
+                        println!("Cannot insert duplicate into binary search tree.");
+                    }
+                }
+            },
+            None => {
+                *root = Some(Box::new(TreeNode::new(value)));
+            }
+        }
+    }
+}
+
+
+struct TreeNode<T> {
+    value: T,
+    left: Option<Box<TreeNode<T>>>,
+    right: Option<Box<TreeNode<T>>>
+}
+
+
+impl<T: Ord> TreeNode<T> {
+    fn new(value: T) -> TreeNode<T> {
+        TreeNode{value: value, left: None, right: None}
+    }
 }
 
 
 fn main() {
-    let tree1 = Box::new(
-        BST::Node(5, Box::new(BST::Leaf), Box::new(BST::Leaf)));
-    let tree2 = insert(tree1, 3);
-    let tree3 = insert(tree2, 7);
+    let mut tree = BST::new();
+    tree.insert(5);
+    tree.insert(3);
+    tree.insert(7);
 
-    let unboxed_root = *tree3;
-
-    match unboxed_root {
-        BST::Node(value, left, right) => {
-            println!("Root is {}.", value);
-
-            let unboxed_left = *left;
-            let unboxed_right = *right;
-
-            match unboxed_left {
-                BST::Node(left_value, _, _) => {
-                    println!("Left child is {}.", left_value);
-                },
-                _ => {}
-            };
-
-            match unboxed_right {
-                BST::Node(right_value, _, _) => {
-                    println!("Right child is {}.", right_value);
-                },
-                _ => {}
-            };
-        },
-        _ => {}
-    }
+    println!("Contains 5?: {}", tree.contains(5));
+    println!("Contains 3?: {}", tree.contains(3));
+    println!("Contains 4?: {}", tree.contains(4));
+    println!("Contains 7?: {}", tree.contains(7));
 }
