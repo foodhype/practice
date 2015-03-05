0cf77cfd26b3d5cc71f80afdcfbf8046923c84fa
foodhype <brianschmitz.main@gmail.com>
2015-03-05 17:48:26 -0600

    Added I/O boilerplate

--- templates/main.py
+++ templates/main.py
@@ -1,8 +1,20 @@
 from sys import stdin
 
+
 def main():
-  lines = stdin
-    
+    t = read(int)
+    for _ in xrange(t):
+        
+
+def read_line(t, delim=" "):
+    """Read delim-separated line of elements of type t from stdin."""
+    return map(t, stdin.next().rstrip().split(delim))
+
+
+def read(t):
+    """Read line containing element of type t."""
+    return t(stdin.next().rstrip())
+
 
 main()
 
